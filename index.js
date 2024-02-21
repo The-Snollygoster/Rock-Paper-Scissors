ee91e571c2f9ff96045c6811017eebb6fb4d48a8
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-02-21 18:32:56 +0000

    Complete task and expand
    I have finished editing the code so that it tracks
    game winners and has a 'first to five' system in
    place. It displays the winner each round and game
    winner.
    
    I've started to add rudimentary animations. I have
    made the elements of the page fade in, staggered.
    There are one or two tweaks, and then some more
    hefty animations I'd like to try and add before I
    am done.

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const header = document.querySelector('.header');
+const headerTwo = document.querySelector('.headerTwo');
+const container = document.querySelector('.container');
+const selectors = document.querySelector('#selectors');
+const finalResult = document.querySelector('#winner');
+const gameWinner = document.querySelector('#gameWinner');
+let playerWins = 0;
+let compWins = 0;
+let ties = 0;
+let result = '';
+
 function playRound(input) {
     function getComputerChoice() {
         function getRandomInt(max) {
             return Math.floor(Math.random()*max);
         }
 
-        let randomInt = getRandomInt(3) 
+        let randomInt = getRandomInt(3);
                 
         if (randomInt === 0) {
             computerChoice = 'rock';
@@ -13,111 +24,60 @@ function playRound(input) {
         } else {
             computerChoice = 'scissors';
         }
-        return computerChoice
+        return computerChoice;
     }
     let x = getComputerChoice();
-    // function getPlayerChoice() {
-    //     let input = prompt('Pick rock, paper or scissors.');
-    //     input = input.toLowerCase()
-    //     if (input === 'rock'||input === 'paper'||input === 'scissors') {
-    //         playerChoice = input;
-    //     } else {
-    //         alert('You must pick rock, paper or scissors!')
-    //         playerChoice = getPlayerChoice()
-    //     }
-    //     return playerChoice.toLowerCase();
-    // }
-    // let y = getPlayerChoice();
-
-    let y = input
+    let y = input;
     let result = y+x;
     let winner = 'tie';
-    const selectors = document.querySelector('#selectors');
-    const finalResult = document.querySelector('#winner');
-    selectors.textContent = `You chose ${y}, the computer chose ${x}`
+    selectors.textContent = `You chose ${y}, the computer chose ${x}`;
     if (result === 'rockscissors' || result === 'paperrock' || result === 'scissorspaper') {
         winner = 'You won!';
+        playerWins++;
     } else if (result === 'rockpaper' || result === 'paperscissors' || result === 'scissorsrock') {
         winner = 'You lost.';
+        compWins++;
     } else {
         winner = 'A tie!';
+        ties++;
     }
+
+    if (playerWins == 5 || compWins == 5) {
+        if (playerWins > compWins) {
+            result = 'Congratulations! You won the game ' + playerWins + ' to ' + compWins + ' with ' + ties + ' ties';
+            } else if (compWins > playerWins) {
+            result = 'Commiserations. You lost the game ' + compWins + ' to ' + playerWins + ' with ' + ties + ' ties';
+            } else {
+            result = 'Wow, a tie! ' + playerWins + ' to ' + compWins + ' with ' + ties + ' ties';
+            }
+        playerWins = 0;
+        compWins = 0;
+        ties = 0;
+        return gameWinner.textContent = result;
+    };
     return finalResult.textContent = winner;
 }
 
-    const rock = document.querySelector('#rock');
-    rock.addEventListener('click', () => {
-        playRound('rock')
-    });
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    selectors.textContent = '';
+    finalResult.textContent = '';
+    gameWinner.textContent = '';
+    playRound('rock');
+});
 
-    const paper = document.querySelector('#paper');
-    paper.addEventListener('click', () => {
-        playRound('paper')
-    });
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    selectors.textContent = '';
+    finalResult.textContent = '';
+    gameWinner.textContent = '';
+    playRound('paper');
+});
 
-    const scissors = document.querySelector('#scissors');
-    scissors.addEventListener('click', () => {
-        playRound('scissors')
-    });
-
-    // This function plays an entire game which consists of 5 rounds and tallies the results to return an overall winner I don't know loops yet so I'm just going to call it 5 times.
-    // function playGame() {
-    //     let playerWins = 0
-    //     let compWins = 0
-    //     let ties = 0
-    //     // Game 1
-    //     let winner = playRound()
-    //     if (winner === 'You won!') {
-    //         playerWins++
-    //     } else if (winner === 'You lost.'){
-    //         compWins++
-    //     } else {
-    //         ties++
-    //     }
-    //     // Game 2
-    //     winner = playRound()
-    //     if (winner === 'You won!') {
-    //         playerWins++
-    //     } else if (winner === 'You lost.'){
-    //         compWins++
-    //     } else {
-    //         ties++
-    //     }
-    //     // Game 3
-    //     winner = playRound()
-    //     if (winner === 'You won!') {
-    //         playerWins++
-    //     } else if (winner === 'You lost.'){
-    //         compWins++
-    //     } else {
-    //         ties++
-    //     }
-    //     // Game 4
-    //     winner = playRound()
-    //     if (winner === 'You won!') {
-    //         playerWins++
-    //     } else if (winner === 'You lost.'){
-    //         compWins++
-    //     } else {
-    //         ties++
-    //     }
-    //     // Game 5
-    //     winner = playRound()
-    //     if (winner === 'You won!') {
-    //         playerWins++
-    //     } else if (winner === 'You lost.'){
-    //         compWins++
-    //     } else {
-    //         ties++
-    //     }
-    //     // This compares the tallies to see who was the overall game winner and gives additional output such as the score and potential ties.
-    //     let gameWinner = 'Unknown'        
-    //     if (playerWins > compWins) {
-    //         gameWinner = 'Congratulations! You won the game ' + playerWins + ' to ' + compWins + ' with ' + ties + ' ties';
-    //     } else if (compWins > playerWins) {
-    //         gameWinner = 'Commiserations. You lost the game ' + compWins + ' to ' + playerWins + ' with ' + ties + ' ties';
-    //     } else {
-    //         gameWinner = 'Wow, a tie! ' + playerWins + ' to ' + compWins + ' with ' + ties + ' ties';
-    //     }
-    //     return gameWinner
-    // }
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    selectors.textContent = '';
+    finalResult.textContent = '';
+    gameWinner.textContent = '';
+    playRound('scissors');
+});
